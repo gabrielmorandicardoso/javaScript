5492ce050fae27d9bad7fb6f78c0824b18b0ef92
Gabriel Morandi Cardoso <110923369+gabrielmorandicardoso@users.noreply.github.com>
2022-09-19 16:46:26 -0300

    aula js

---
+++ aula08/ex001.js
@@ -0,0 +1,7 @@
+console.log('Olá testando o console.')
+var vel = 60
+console.log('A velocidade do seu carro é de ' + vel + 'Km/h')
+if( vel > 60) {
+    console.log('A sua velocidade é maior que a permitida, você foi multado!')
+}
+console.log('Dirija com cuidado')
