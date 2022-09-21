fa2bc1081eef4f08a693e79431c0c6164bea1e2b
Gabriel Morandi Cardoso <110923369+gabrielmorandicardoso@users.noreply.github.com>
2022-09-21 10:57:39 -0300

    Update script.js

--- aula009/praticando03/scripts/script.js
+++ aula009/praticando03/scripts/script.js
@@ -1,10 +1,43 @@
 function verificar() {
     var data = new Date()
-    var anoAtual = data.getFullYear()
-    var anoNas = document.getElementById('txt1')
+    var ano = data.getFullYear()
+    var nas = document.getElementById('txt1')
     var res = document.getElementById('res')
-    var analise = anoNas - Number.anoAtual
 
+
+    if (nas.value.length == 0 || nas.value > ano) {
+        res.innerHTML = "Não está correto os campos preenchidos, tente novamente!"
+        } else {
+        var fSex = document.getElementsByName("sex")
+        var idade = ano - Number(nas.value)
+        var genero = ''
+        var imagem = document.createElement('img')
+        imagem.setAttribute('id', 'foto')
+        if (fSex[0].checked) {
+            genero = 'homem'
+            if (idade > 0 && idade < 13) {
+                var tipo = 'Criança'
+            } else if (idade < 25) {
+                var tipo = "Jovem"
+            } else if (idade < 59) {
+                var tipo = "Adulto"
+            } else {
+                var tipo = "Idoso"
+            }
+        } else if (fSex[1].checked) {
+            genero = 'mulher'
+            if (idade > 0 && idade < 13) {
+                var tipo = 'Criança'
+            } else if (idade < 25) {
+                var tipo = "Jovem"
+            } else if (idade < 59) {
+                var tipo = "Adulto"
+            } else {
+                var tipo = "Idoso"
+            }
+        }
+    }
+
+        res.innerHTML = 'Analisamos a sua idade, você tem ' + idade + ' anos.' + 'Ja é considerado um ' + tipo
 }
 
-res.innerHTML = analise
