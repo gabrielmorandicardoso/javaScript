72b2aad368841b14881f7dcec0be9284e2437775
Gabriel Morandi Cardoso <110923369+gabrielmorandicardoso@users.noreply.github.com>
2022-09-21 14:35:15 -0300

    Create repeticao.js

---
+++ aula010/repeticao.js
@@ -0,0 +1,15 @@
+var c = 1
+do {
+    console.log( c + "°" + ' pedaço de pizza')
+    c++
+} while(c <= 8)
+
+
+
+/*
+var c = 1
+while(c <= 8) {
+    console.log( c + "°" + ' pedaço de pizza')
+    c++
+}
+*/
