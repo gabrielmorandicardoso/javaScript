93ea8758177fdc0118b11b34b23b7ddd0eb6cafc
Gabriel Morandi Cardoso <110923369+gabrielmorandicardoso@users.noreply.github.com>
2022-09-27 12:10:51 -0300

    exercicio

--- aula15/ex01/scripts/script.js
+++ aula15/ex01/scripts/script.js
@@ -26,7 +26,38 @@
             let item = document.createElement('option')
             item.text = `Valor ${num.value} foi adicionado`
             lista.append(item)
+            res.innerHTML = ''
         } else {
             window.alert('Valor inválido ou ja encontrado na lista.')
+        }
+
+        num.value = ''
+        num.focus()
+    }
+
+    function finalizar() {
+        if (valores.length == 0) {
+            window.alert('Adicione os valores antes de finalizar!')
+        } else {
+            let tot = valores.length
+            let maior = valores[0]
+            let menor = valores[0]
+            let soma = 0
+            let media = 0
+            for (let pos in valores) {
+                soma += valores[pos]
+                if (valores[pos] > maior)
+                    maior = valores[pos]
+                    if (valores[pos] < menor)
+                        menor = valores[pos]
+            }
+
+            media = soma / tot
+            res.innerHTML = ''
+            res.innerHTML +=`<p>    Ao todo, temos ${tot} números cadastrados </p>`
+            res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+            res.innerHTML += `<p>o menor valor informado foi ${menor}.</p>`
+            res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+            res.innerHTML += `<p>A média entre os valores digitados é ${media}.</p>`
         }
     }
